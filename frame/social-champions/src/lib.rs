d17b41704c19639ee6fca4c1396cb8a85d648c62
Andrey Orlov <aaorlov1@gmail.com>
2021-04-27 12:07:01 +0400

    Update pallet-social-champions

--- frame/social-champions/src/lib.rs
+++ frame/social-champions/src/lib.rs
@@ -4,6 +4,7 @@ use frame_support::{
     decl_error, decl_event, decl_module, decl_storage, dispatch, ensure, traits::Get,
 };
 use frame_system::ensure_signed;
+use pallet_staking::EraIndex;
 use sp_std::prelude::*;
 
 #[cfg(test)]
@@ -12,14 +13,24 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-pub trait Config: frame_system::Config + pallet_assets::Config {
+pub trait Config: frame_system::Config + pallet_assets::Config + pallet_staking::Config {
     type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
 }
 
 decl_storage! {
     trait Store for Module<T: Config> as ValidatorRegistry {
-        ChampionOf get(fn social_of): map hasher(blake2_128_concat) T::AccountId => T::AssetId;
-        Validators get(fn validators): map hasher(blake2_128_concat) T::AssetId => Vec<T::AccountId>;
+        /// Map from the controller account to the social token id.
+        pub ChampionOf get(fn champion_of): map hasher(blake2_128_concat) T::AccountId => T::AssetId;
+        /// Map from the social token id to the vector of controller accounts.
+        ChampionsOfSocialToken get(fn champions_of_social_token): map hasher(blake2_128_concat) T::AssetId => Vec<T::AccountId>;
+        /// Current champions (controller accounts).
+        Champions get(fn champions): Vec<T::AccountId>;
+        /// Map from the era index to the vector of controller accounts.
+        ChampionHistory get(fn champion_history): map hasher(blake2_128_concat) EraIndex => Vec<T::AccountId>;
+        /// Map from (era index, controller account) to the social token id.
+        ChampionDetailHistory get(fn champion_detail_history): double_map hasher(twox_64_concat) EraIndex, hasher(twox_64_concat) T::AccountId => T::AssetId;
+        /// Number of eras to keep in history.
+        HistoryDepth get(fn history_depth): u32 = 84;
     }
 }
 
@@ -55,7 +66,10 @@ decl_module! {
             ensure!(!<ChampionOf<T>>::contains_key(&validator), Error::<T>::AlreadyRegistered);
 
             <ChampionOf<T>>::insert(&validator, social_token_id);
-            <Validators<T>>::mutate(social_token_id, |validators| {
+            <ChampionsOfSocialToken<T>>::mutate(social_token_id, |validators| {
+                validators.push(validator.clone())
+            });
+            <Champions<T>>::mutate(|validators| {
                 validators.push(validator.clone())
             });
 
@@ -66,17 +80,48 @@ decl_module! {
         #[weight = 10_000 + T::DbWeight::get().writes(1)]
         pub fn unregister(origin) -> dispatch::DispatchResult {
             let validator = ensure_signed(origin)?;
+            let social_token_id = <ChampionOf<T>>::try_get(&validator)
+                .map_err(|_| Error::<T>::NotFound)?;
 
-            ensure!(<ChampionOf<T>>::contains_key(&validator), Error::<T>::NotFound);
-
-            let social_token_id = <ChampionOf<T>>::get(&validator);
             <ChampionOf<T>>::remove(&validator);
-            <Validators<T>>::mutate(social_token_id, |validators| {
+            <ChampionsOfSocialToken<T>>::mutate(social_token_id, |validators| {
+                validators.retain(|account_id| account_id != &validator)
+            });
+            <Champions<T>>::mutate(|validators| {
                 validators.retain(|account_id| account_id != &validator)
             });
 
             Self::deposit_event(RawEvent::Unregistered(validator, social_token_id));
             Ok(())
         }
+
+        fn on_finalize() {
+            let current_era = pallet_staking::CurrentEra::get().unwrap_or(0);
+            Self::clean_history(current_era);
+            Self::update_history(current_era);
+        }
+
+    }
+}
+
+impl<T: Config> Module<T> {
+    fn clean_history(current_era: EraIndex) {
+        let history_depth = HistoryDepth::get();
+        match current_era.checked_sub(history_depth) {
+            Some(era) => {
+                <ChampionHistory<T>>::remove(era);
+                <ChampionDetailHistory<T>>::remove_prefix(era);
+            }
+            None => (),
+        }
+    }
+
+    fn update_history(current_era: EraIndex) {
+        let champions = <Champions<T>>::get();
+        champions.iter().for_each(|champion| {
+            let social_token_id = <ChampionOf<T>>::get(champion);
+            <ChampionDetailHistory<T>>::insert(current_era, champion, social_token_id)
+        });
+        <ChampionHistory<T>>::insert(current_era, champions);
     }
 }
--- frame/social-champions/src/mock.rs
+++ frame/social-champions/src/mock.rs
@@ -1,5 +1,5 @@
 use crate as pallet_social_champions;
-use frame_support::{parameter_types, traits::GenesisBuild};
+use frame_support::parameter_types;
 use sp_core::H256;
 use sp_runtime::{
     testing::Header,
@@ -8,6 +8,8 @@ use sp_runtime::{
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
+type AccountId = u64;
+type Balance = u64;
 
 frame_support::construct_runtime!(
     pub enum Test where
@@ -18,38 +20,40 @@ frame_support::construct_runtime!(
         System: frame_system::{Module, Call, Config, Storage, Event<T>},
         Assets: pallet_assets::{Module, Call, Storage, Event<T>},
         Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>},
+        Session: pallet_session::{Module, Call, Storage, Event, Config<T>},
+        Staking: pallet_staking::{Module, Call, Storage, Config<T>, Event<T>},
         SocialChampions: pallet_social_champions::{Module, Call, Storage, Event<T>},
     }
 );
 
 parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub BlockWeights: frame_system::limits::BlockWeights =
-		frame_system::limits::BlockWeights::simple_max(1024);
+    pub const BlockHashCount: u64 = 250;
+    pub BlockWeights: frame_system::limits::BlockWeights =
+        frame_system::limits::BlockWeights::simple_max(1024);
 }
 impl frame_system::Config for Test {
-	type BaseCallFilter = ();
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type Origin = Origin;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Call = Call;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64; // u64 is not enough to hold bytes used to generate bounty account
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = BlockHashCount;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = pallet_balances::AccountData<u64>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ();
+    type BaseCallFilter = ();
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type Origin = Origin;
+    type Index = u64;
+    type BlockNumber = u64;
+    type Call = Call;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64; // u64 is not enough to hold bytes used to generate bounty account
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = Event;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<u64>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
 }
 
 parameter_types! {
@@ -88,6 +92,107 @@ impl pallet_assets::Config for Test {
     type WeightInfo = ();
 }
 
+parameter_types! {
+    pub const MinimumPeriod: u64 = 5;
+}
+impl pallet_timestamp::Config for Test {
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type MinimumPeriod = MinimumPeriod;
+    type WeightInfo = ();
+}
+
+impl pallet_session::historical::Config for Test {
+    type FullIdentification = pallet_staking::Exposure<AccountId, Balance>;
+    type FullIdentificationOf = pallet_staking::ExposureOf<Test>;
+}
+
+sp_runtime::impl_opaque_keys! {
+    pub struct SessionKeys {
+        pub foo: sp_runtime::testing::UintAuthorityId,
+    }
+}
+
+pub struct TestSessionHandler;
+impl pallet_session::SessionHandler<AccountId> for TestSessionHandler {
+    const KEY_TYPE_IDS: &'static [sp_runtime::KeyTypeId] = &[];
+
+    fn on_genesis_session<Ks: sp_runtime::traits::OpaqueKeys>(_validators: &[(AccountId, Ks)]) {}
+
+    fn on_new_session<Ks: sp_runtime::traits::OpaqueKeys>(
+        _: bool,
+        _: &[(AccountId, Ks)],
+        _: &[(AccountId, Ks)],
+    ) {
+    }
+
+    fn on_disabled(_: usize) {}
+}
+
+impl pallet_session::Config for Test {
+    type SessionManager = pallet_session::historical::NoteHistoricalRoot<Test, Staking>;
+    type Keys = SessionKeys;
+    type ShouldEndSession = pallet_session::PeriodicSessions<(), ()>;
+    type NextSessionRotation = pallet_session::PeriodicSessions<(), ()>;
+    type SessionHandler = TestSessionHandler;
+    type Event = Event;
+    type ValidatorId = AccountId;
+    type ValidatorIdOf = pallet_staking::StashOf<Test>;
+    type DisabledValidatorsThreshold = ();
+    type WeightInfo = ();
+}
+
+pallet_staking_reward_curve::build! {
+    const I_NPOS: sp_runtime::curve::PiecewiseLinear<'static> = curve!(
+        min_inflation: 0_025_000,
+        max_inflation: 0_100_000,
+        ideal_stake: 0_500_000,
+        falloff: 0_050_000,
+        max_piece_count: 40,
+        test_precision: 0_005_000,
+    );
+}
+parameter_types! {
+    pub const RewardCurve: &'static sp_runtime::curve::PiecewiseLinear<'static> = &I_NPOS;
+    pub const MaxNominatorRewardedPerValidator: u32 = 64;
+    pub const UnsignedPriority: u64 = 1 << 20;
+}
+
+pub type Extrinsic = sp_runtime::testing::TestXt<Call, ()>;
+
+impl<C> frame_system::offchain::SendTransactionTypes<C> for Test
+where
+    Call: From<C>,
+{
+    type OverarchingCall = Call;
+    type Extrinsic = Extrinsic;
+}
+
+impl pallet_staking::Config for Test {
+    type Currency = Balances;
+    type UnixTime = pallet_timestamp::Module<Self>;
+    type CurrencyToVote = frame_support::traits::SaturatingCurrencyToVote;
+    type RewardRemainder = ();
+    type Event = Event;
+    type Slash = ();
+    type Reward = ();
+    type SessionsPerEra = ();
+    type SlashDeferDuration = ();
+    type SlashCancelOrigin = frame_system::EnsureRoot<Self::AccountId>;
+    type BondingDuration = ();
+    type SessionInterface = Self;
+    type RewardCurve = RewardCurve;
+    type NextNewSession = Session;
+    type ElectionLookahead = ();
+    type Call = Call;
+    type MaxNominatorRewardedPerValidator = MaxNominatorRewardedPerValidator;
+    type UnsignedPriority = UnsignedPriority;
+    type MaxIterations = ();
+    type MinSolutionScoreBump = ();
+    type OffchainSolutionWeightLimit = ();
+    type WeightInfo = ();
+}
+
 impl pallet_social_champions::Config for Test {
     type Event = Event;
 }
@@ -104,11 +209,17 @@ impl Default for ExtBuilder {
 
 impl ExtBuilder {
     pub fn build(self) -> sp_io::TestExternalities {
-        let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+        let mut t = frame_system::GenesisConfig::default()
+            .build_storage::<Test>()
+            .unwrap();
         let (owner, admin, max_zombies, min_balance) = (1, 1, 0, 100);
         pallet_assets::GenesisConfig::<Test> {
-            assets: (1..=17).map(|id| (id, owner, admin, max_zombies, min_balance)).collect(),
-        }.assimilate_storage(&mut t).unwrap();
+            assets: (1..=17)
+                .map(|id| (id, owner, admin, max_zombies, min_balance))
+                .collect(),
+        }
+        .assimilate_storage(&mut t)
+        .unwrap();
         let mut ext = sp_io::TestExternalities::new(t);
         ext.execute_with(|| System::set_block_number(1));
         ext
